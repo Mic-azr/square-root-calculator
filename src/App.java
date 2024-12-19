da27e94e2017eb0954d8bdeaf427dbd9c10ff2ef
Mic-azr <michael.cummings1997@gmail.com>
2024-12-19 17:07:49 -0500

    Minor refactor

--- src/App.java
+++ src/App.java
@@ -1,27 +1,20 @@
 import java.lang.Math;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        double num1 = 49;
-        double num2 = 50;
-        double num3 = 48;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        double userNum = scanner.nextDouble();
+        scanner.close();
 
-        System.out.println(Math.sqrt(num1));
+        double result = Math.sqrt(userNum);
 
-        if (Math.sqrt(num1) % 1 == 0) {
+        System.out.println(result);
+
+        if (result % 1 == 0) {
             System.out.println("Your number is a perfect square!");
         }
 
-        System.out.println(Math.sqrt(num2));
-
-        if (Math.sqrt(num2) % 1 == 0) {
-            System.out.println("Your number is a perfect square!");
-        }
-
-        System.out.println(Math.sqrt(num3));
-
-        if (Math.sqrt(num3) % 1 == 0) {
-            System.out.println("Your number is a perfect square!");
-        }
     }
 }
